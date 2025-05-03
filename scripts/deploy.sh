19fb70a5b798470ebfeaa25fb94f11228f155b7f
Wendell Bitencourt <bat.delgeorge@hotmail.com>
2025-05-03 11:54:36 -0300

    Deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
+clear
+
+start_time=$(date +%s)
+
 npm run build
 
 git add .
 git commit -m "Deploy"
+
+new_version=$(npm version patch)
+
 git push
 
+end_time=$(date +%s)
+
+execution_time=$((end_time - start_time))
+
+echo
+echo
+echo
+echo
+echo
+echo "Total execution global time: $execution_time seconds"
+echo
+echo
+echo
+echo
+echo
+echo
